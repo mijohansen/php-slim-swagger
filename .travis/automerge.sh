cfad0eb358500b0cc412eef6c5592fe86b1ba862
Michael Johansen <michael@redperformance.no>
2018-11-20 01:52:20 +0100

    fixin automerge

--- .travis/automerge.sh
+++ .travis/automerge.sh
@@ -28,14 +28,16 @@ printf 'Merging %s\n' "$TRAVIS_COMMIT" >&2
 git merge --ff-only "$TRAVIS_COMMIT"
 
 export SEMVER_LAST_TAG=$(git describe --abbrev=0 --tags 2>/dev/null)
-export LAST_COMMIT_DATE=$(git log -1 --date=short --pretty=format:%cd)
-export TODAYS_DATE=$(date +%Y-%m-%d)
 
 if [[ -z $SEMVER_LAST_TAG ]]; then
     >&2 echo "No tags defined creating a new one"
+    LAST_COMMIT_DATE=$TODAYS_DATE
     SEMVER_LAST_TAG="0.0.1"
+else
+    LAST_COMMIT_DATE=$(git log -1 --date=short --pretty=format:%cd $SEMVER_LAST_TAG)
 fi
 
+export TODAYS_DATE=$(date +%Y-%m-%d)
 # To avoid to many versions use the same tag at a certain day
 if [[ $LAST_COMMIT_DATE != $TODAYS_DATE ]]; then
     >&2 echo "Bumping tag"
@@ -44,7 +46,7 @@ else
     >&2 echo "Will use todays patch, just delete it from master. ${SEMVER_LAST_TAG}"
     SEMVER_NEW_TAG=$SEMVER_LAST_TAG
     if [[ $SEMVER_LAST_TAG != "0.0.1" ]]; then
-        git push "$push_uri" :"$SEMVER_LAST_TAG"
+        git push "$push_uri" :refs/tags/$SEMVER_LAST_TAG
     fi
 fi
 
